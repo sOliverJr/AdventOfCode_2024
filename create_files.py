fbc0662cef61953de4dd81a78e8fdabcab59e9da
d0298891 <oliverseider@yahoo.de>
2024-12-13 13:00:26 +0100

    updated create_files.py

--- create_files.py
+++ create_files.py
@@ -1,16 +1,35 @@
 import os
 
-# Specify the directory name
 current_day = '09'
-
-# Create the directory
 directory_name = f'Day{current_day}'
+
+part1 = f"""CURRENT_DAY = '{current_day}'
+
+
+def read_input() -> list:
+    input = []
+    file = open(f'day{ current_day }_input.txt', 'r')
+    for line in file:
+        line = line.strip('\\n')
+        input.append(list(line))
+
+    return input
+
+
+result = 0
+
+# Logik
+
+print('Result:' + str(result))
+# First run & Try :)
+"""
+
 try:
     os.mkdir(directory_name)
     open(f'{directory_name}/day{current_day}_input.txt', "a").close()
     open(f'{directory_name}/day{current_day}_part2.py', "a").close()
     f = open(f'{directory_name}/day{current_day}_part1.py', "a")
-    f.write(open("Day08/day08_part1.py", "r").read())
+    f.write(part1)
     f.close()
 except Exception as e:
     print(f"An error occurred: {e}")
